4ffffb7edb850d10ab7edc84c1d21f1c1c282230
Ruslan <58813459+Ruslasadov@users.noreply.github.com>
2022-09-18 01:15:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 chislo = 546
 stroka = 'Моя первая программа'
 spisok = [chislo,stroka]
-slovar = {chislo:stroka}
+slovar = {chislo:chislo,stroka:stroka}
 konkatenaciya = stroka + ' ' + str(chislo*234)
 print(chislo,stroka,spisok,slovar,konkatenaciya)
