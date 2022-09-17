db319ff6dfe1f2fcf6a07f9d9abe4024b7303998
Ruslan <58813459+Ruslasadov@users.noreply.github.com>
2022-09-18 01:12:40 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+chislo = 546
+stroka = 'Моя первая программа'
+spisok = [chislo,stroka]
+slovar = {chislo:stroka}
+konkatenaciya = stroka + ' ' + str(chislo*234)
+print(chislo,stroka,spisok,slovar,konkatenaciya)
