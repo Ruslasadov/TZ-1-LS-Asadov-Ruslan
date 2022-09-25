66a59365812bd914d27ab554dde69bffbb426e55
Ruslan <58813459+Ruslasadov@users.noreply.github.com>
2022-09-25 17:32:48 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-chislo = 546
-stroka = 'Моя первая программа'
-spisok = [chislo,stroka]
-slovar = {chislo:chislo,stroka:stroka}
-konkatenaciya = stroka + ' ' + str(chislo*234)
-print(chislo,stroka,spisok,slovar,konkatenaciya)
+# Задание #2. Программа на условия, цикл и сравнение.
+# 1)
+letters = 'who keeps company with the wolf, will learn to howl.'
+print( letters .count('w'))
+
+# 2)
+
+letters = 'Who keeps company with the wolf, will learn to howl.'
+for x in letters:
+    if x != 'l':
+        print (x)
